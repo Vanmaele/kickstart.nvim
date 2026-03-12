dcd6b01503a0a211f9e0093ec6e94dc4fc7ee92b
Aaron Vanmaele <aaron.vanmaele@ledsgo.eu>
2026-03-12 16:17:59 +0100

    Added vim-fugitive

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,16 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+---
+---
+return {
+
+  {
+    'tpope/vim-fugitive',
+    keys = {
+      { '<leader>gg', '<cmd>Git<CR>', desc = 'Git status' },
+      { '<leader>gd', '<cmd>Gdiffsplit<CR>', desc = 'Git diff split' },
+      { '<leader>gb', '<cmd>Git blame<CR>', desc = 'Git blame' },
+    },
+  },
+}
